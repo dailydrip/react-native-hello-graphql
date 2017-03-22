298572dc80fcc56323aec1fc06c1808bb3d36920
Franzé Jr <franzejr@gmail.com>
2017-03-22 13:47:03 -0300

    Use AppContainer

---
+++ App/AppContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import App from './App'
+
+export default class AppContainer extends Component {
+  render() {
+    return (
+      <App />
+    );
+  }
+}
+
